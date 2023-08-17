84ba077ad926b834428b221a07f27dfe71839ab6
unknown <ansariawaisraees@gmail.com>
2023-08-17 23:56:19 +0530

    Adding Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+//        obj.name = "John"; // This will result in an error
+//        System.out.println(obj.name); // This will also result in an error
+
+        obj.setName("Alice");
+        System.out.println(obj.getName());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
+import javax.naming.Name;
+
 public class RWOnly {
-    
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
 }
